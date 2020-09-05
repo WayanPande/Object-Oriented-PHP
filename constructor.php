bead1caed90104759092dd475098117ccc3c1815
WayanPande <yande554@gmail.com>
2020-09-05 20:19:00 +0800

    Menambahkan file constructor dan materi constructor

---
+++ constructor.php
@@ -0,0 +1,36 @@
+<?php
+
+class Produk
+{
+    public $judul,
+        $penulis,
+        $penerbit,
+        $harga;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel()
+    {
+
+        return "$this->judul, $this->penerbit";
+    }
+}
+
+
+$produk1 = new Produk("Naruto", "saya", "Gramedia", 5000);
+
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Playstation", 250000);
+
+$produk3 = new Produk("Mishi");
+
+echo "Komik: " . $produk1->getLabel();
+echo "<br>";
+echo "Game: " . $produk2->getLabel();
+echo "<br>";
+echo "Game: " . $produk3->getLabel();
