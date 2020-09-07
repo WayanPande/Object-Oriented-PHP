b31e4272899038c4e57cb6e34ca52ad2d0dea452
WayanPande <yande554@gmail.com>
2020-09-07 20:24:18 +0800

    Materi mengenai constant/konstanta

---
+++ constant.php
@@ -0,0 +1,27 @@
+<?php
+
+// define('NAMA', 'lojik aih');
+
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 12;
+
+// echo UMUR;
+
+// class Coba
+// {
+//     const NAMA = "Wadidwa";
+// }
+
+// echo Coba::NAMA;
+
+// echo __FILE__;
+
+function coba()
+{
+    return __FUNCTION__;
+}
+
+echo coba();
