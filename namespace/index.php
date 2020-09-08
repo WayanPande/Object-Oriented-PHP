d34a853ac5f1a938d2ba188d017b18fe05b16fc9
WayanPande <yande554@gmail.com>
2020-09-08 20:30:19 +0800

    Membuat Namespace

---
+++ namespace/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'App/init.php';
+
+// $produk1 = new Komik("Naruto", "saya", "Gramedia", 5000, 100);
+
+// $produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Playstation", 250000, 50);
+
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk($produk1);
+// $cetakProduk->tambahProduk($produk2);
+// echo $cetakProduk->cetak();
+
+use App\Service\User as ServiceUser;
+use App\Produk\User as ProdukUser;
+
+
+new ServiceUser();
+echo "<br>";
+new ProdukUser();
